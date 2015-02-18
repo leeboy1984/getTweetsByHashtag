b5dc9cda6ecc2c90f6520c2a44be759a06fcfb61
Abel Zafra <abel.zafra@gmail.com>
2015-02-18 20:36:25 +0100

    Added basic config EMPTY parameters

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 $config['consumer_key'] = "";
 $config['consumer_secret'] = "";
-$config['bearer_token'] = "";
-$config['bearer_encoded'] = "";
+$config['bearer_token'] = $config['consumer_key'].':'.$config['consumer_secret'];
+$config['bearer_encoded'] = base64_encode($config['bearer_token']);
+$config['twitter_search_api'] = "https://api.twitter.com/1.1/search/tweets.json?q=";
+$config['max_tweets'] = 100;
 
 ?>
