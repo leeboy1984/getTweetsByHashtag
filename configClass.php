894266166982f31d069072b89799300e6d0cd34c
Abel <abel.zafra@gmail.com>
2016-09-20 12:36:20 +0200

    Update configClass.php

--- configClass.php
+++ configClass.php
@@ -2,18 +2,18 @@
 class ConfigClass {
 
 	//Twitter Config
-	var $consumer_key = "xvz1evFS4wEEPTGEFPHBog";
-	var $consumer_secret = "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg";
+	var $consumer_key = "consumer_key";
+	var $consumer_secret = "consumer_secret";
 	var $twitter_search_api = "https://api.twitter.com/1.1/search/tweets.json?q=";
 	var $twitter_auth_url = 'https://api.twitter.com/oauth2/token';
 	var $twitter_grants = 'client_credentials';
 	var $max_tweets = 100;
 
 	// DB Config
-    var $dbHost = 'localhost';
-    var $dbUser = 'root';
-    var $dbPass = 'root';
-    var $dbName = 'critizen_tweets';
+    var $dbHost = 'db_host';
+    var $dbUser = 'db_user';
+    var $dbPass = 'db_pass';
+    var $dbName = 'db_dbname';
 
     function getBearerToken(){
     	return $this->consumer_key.':'.$this->consumer_secret;
