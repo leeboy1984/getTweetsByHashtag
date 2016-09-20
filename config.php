6a5f222bc03e493aaef20cbc86b486b7ff2e0df1
Abel <abel.zafra@gmail.com>
2016-09-20 12:35:35 +0200

    Removing keys

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$config['consumer_key'] = "xvz1evFS4wEEPTGEFPHBog";
-$config['consumer_secret'] = "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg";
+$config['consumer_key'] = "consumer_key";
+$config['consumer_secret'] = "consumer_secret";
 //$config['bearer_token'] = $config['consumer_key'].':'.$config['consumer_secret'];
 //$config['bearer_encoded'] = base64_encode($config['bearer_token']);
 $config['twitter_search_api'] = "https://api.twitter.com/1.1/search/tweets.json?q=";
